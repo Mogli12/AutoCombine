40b58ef5ba683b64bb9d18eff9835d5ce2e32b59
Stefan Biedenstein <mbiedens@gmail.com>
2015-09-23 10:09:27 +0200

    Using the same mogliHud.lua in all mods

--- zzzAutoCombine/mogliHud.lua
+++ zzzAutoCombine/mogliHud.lua
@@ -2,10 +2,11 @@
 -- mogliBasics
 -- This is the specialization for mogliBasics
 --
-local mogliHudVersion = 1.001
+local mogliHudVersion = 1.101
 
 -- change log
 -- 1.00 initial version 
+-- 1.10 always draw title
 
 local mogliHudClass = g_currentModName..".mogliHud"
 
@@ -162,14 +163,14 @@ else
 		function _newClass_:addButton(imgEnabled, imgDisabled, cbOnClick, cbVisible, nx, ny, textEnabled, textDisabled, textCallback, imgCallback)
 			local x = self[_level0_].BtnPosX + (nx-1)*(self[_level0_].BtnWidth+self[_level0_].Border);
 			local y = self[_level0_].BtnPosY - (ny-1)*(self[_level0_].BtnHeight+self[_level0_].Border);
-			local img1 = Utils.getNoNil( imgEnabled, "dds/empty.dds" )
+			local img1 = Utils.getNoNil( imgEnabled, "empty.dds" )
 			local state, result = pcall( Utils.getFilename, img1, self[_level0_].Directory )
 			if not state then
 				print("ERROR: "..tostring(result).." (img1: "..tostring(img1)..")")
 				return
 			end
 			local overlay = Overlay:new(nil, result, x,y,self[_level0_].BtnWidth,self[_level0_].BtnHeight);
-			local img2 = "dds/empty.dds"
+			local img2 = "empty.dds"
 			if imgDisabled ~= nil then
 				img2 = imgDisabled
 			end;
@@ -206,10 +207,10 @@ else
 		function _newClass_:addCloseButton(nx, ny)
 			local x = self[_level0_].BtnPosX + (nx-1)*(self[_level0_].BtnWidth+self[_level0_].Border) + 0.5*self[_level0_].BtnWidth;
 			local y = self[_level0_].BtnPosY + self[_level0_].BtnHeight+self[_level0_].Border;
-			local overlay = Overlay:new(nil, Utils.getFilename("dds/close.dds", self[_level0_].Directory), x,y,0.5*self[_level0_].BtnWidth,0.5*self[_level0_].BtnHeight);
+			local overlay = Overlay:new(nil, Utils.getFilename("close.dds", self[_level0_].Directory), x,y,0.5*self[_level0_].BtnWidth,0.5*self[_level0_].BtnHeight);
 			local button = {enabled=true, ovEnabled=overlay, ovDisabled=nil, onClick=_newClass_.onClose, onVisible=nil, twoState=false, rect={x,y,x+0.5*self[_level0_].BtnWidth,y+0.5*self[_level0_].BtnHeight}, text1 = nil, text2 = nil, textcb = nil, onRender = nil };
 			button.overlays = {}
-			button.overlays["dds/close.dds"] = overlay
+			button.overlays["close.dds"] = overlay
 			if self[_level0_].Buttons == nil then self[_level0_].Buttons = {}; end
 			table.insert(self[_level0_].Buttons, button);
 			return button;
@@ -399,16 +400,25 @@ else
 		------------------------------------------------------------------------
 		-- draw
 		------------------------------------------------------------------------
-		function _newClass_:draw(hideKey)
+		function _newClass_:draw(hideKey,alwaysDrawTitle)
 			if self.isClient and self[_level0_] ~= nil then
+				if hideKey == nil or not hideKey then
+					g_currentMission:addHelpButtonText(_newClass_.getText(self[_level0_].GuiOffTextID), self[_level0_].GuiShowKey);
+				end
+				local showTitle
 				if self[_level0_].GuiActive then
-					if hideKey == nil or not hideKey then
-						g_currentMission:addHelpButtonText(_newClass_.getText(self[_level0_].GuiOffTextID), self[_level0_].GuiShowKey);
-					end
 					g_mouseControlsHelp.active = false;
 					InputBinding.setShowMouseCursor(true);		
-					setTextAlignment(RenderText.ALIGN_LEFT);
-					
+					titlePosY = self[_level0_].TitlePosY
+					showTitle = true
+				else
+					titlePosY = self[_level0_].TextPosY
+					showTitle = alwaysDrawTitle
+				end
+				
+				setTextAlignment(RenderText.ALIGN_LEFT);
+				if showTitle then
+				
 					setTextBold(true);		
 					if self[_level0_].Status == 0 then
 						setTextColor(1,1,1,1);
@@ -420,11 +430,13 @@ else
 						setTextColor(1,0.5,0,1);
 					end
 					
-					renderText(self[_level0_].TextPosX, self[_level0_].TitlePosY, self[_level0_].TextSize ,self[_level0_].Title);		
-					
-					setTextBold(false);		
-					setTextColor(1,1,1,1);
-								
+					renderText(self[_level0_].TextPosX, titlePosY, self[_level0_].TextSize ,self[_level0_].Title);		
+				end
+				
+				setTextBold(false);		
+				setTextColor(1,1,1,1);
+				
+				if self[_level0_].GuiActive then							
 					self[_level0_].Overlay:render();
 					if     self[_level0_].Tooltip           ~= nil and self[_level0_].Tooltip           ~= "" then
 						renderText(self[_level0_].TextPosX, self[_level0_].TextPosY, self[_level0_].TextSize ,self[_level0_].Tooltip);
@@ -432,8 +444,6 @@ else
 						renderText(self[_level0_].TextPosX, self[_level0_].TextPosY, self[_level0_].TextSize ,self[_level0_].InfoText);
 					end
 					_newClass_.renderButtons(self);
-				elseif hideKey == nil or not hideKey then
-					g_currentMission:addHelpButtonText(_newClass_.getText(self[_level0_].GuiOnTextID), self[_level0_].GuiShowKey);
 				end
 			end
 		end
