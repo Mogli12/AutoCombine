88c6f155560f0271ca7e2e395cdd49cc8a7b2d16
Stefan Biedenstein <mbiedens@gmail.com>
2016-08-13 13:59:03 +0200

    v4.1.0.1
    - pre compiled luc files for better performance

--- build.bat
+++ build.bat
@@ -1,4 +1,16 @@
-del zzzAutoCombine.zip
-cd zzzAutoCombine
-"C:\Program Files\WinRAR\winrar" A -r ..\zzzAutoCombine.zip *.*
+setlocal
+set modname=zzzAutoCombine
+del %modname%.zip
+cd %modname%
+for %%I in (..\src\*.lua) do call :loopbody "%%~fI"
+"C:\Program Files\WinRAR\winrar" A -r ..\%modname%.zip *.*
 cd ..
+pause
+goto :EOF
+
+:loopbody
+	echo %~n1
+	del %~n1.luc	
+	call c:\work\luapower-all-master\luajit -bg ..\src\%~n1.lua %~n1.luc	
+	goto :EOF
+
